7e698b35f129d503fd0016815682869595fc6a85
corrupt <804686559@qq.com>
2026-04-13 11:44:06 +0000

    record:归并排序

--- new-Leecode/src/main/java/com/example/y2026/m04/day7/P918.java
+++ new-Leecode/src/main/java/com/example/y2026/m04/day7/P918.java
@@ -2,7 +2,7 @@ package com.example.y2026.m04.day7;
 /** 
  * <h1>环形子数组的最大和</h1>
  */
-public interface P918 {
+public class P918 {
     //先采用暴力解法，枚举起点，计算所有可能
     public int maxSubarraySumCircular (int[] nums) {
         int len = nums.length;
@@ -10,7 +10,7 @@ public interface P918 {
         for(int i = 0 ; i < len ; i++) {
             int sum = nums[i];
             for(int j = 1;j < len; j++) {
-                sum += nums[(i + j) % n];
+                sum += nums[(i + j) % len];
                 max = Math.max(sum, max);
             }
         }
@@ -18,7 +18,7 @@ public interface P918 {
     }
     /*
     结果可能存在两种形式:1.普通的最小子数组和。2.跨越头尾的最小子数组和
-    如果是跨越头尾的最小子数组和，其最大值等价于头尾之间形成的子数组的最小值
+    如果是跨越头尾的最小子数组和，其最大值等价于头尾之间形成的子数组的最小值(注意计算最小值时，可能最小值等于整个数组的和，此时相减后为空子数组，不符合题意)
      */
     public int maxSubarraySumCircularV2(int[] nums) {
         int len = nums.length;
@@ -33,7 +33,7 @@ public interface P918 {
             }
         }
         //求普通数组的最小子数组和
-        int min = 0;
+        int min = Integer.MAX_VALUE;
         sum = 0;
         int all = 0;
         for(int i = 0 ; i < len ; i++) {
@@ -44,6 +44,7 @@ public interface P918 {
                 sum = 0;
             }
         }
+        if (sum == all) return max;
         return Math.max(max, all - min);
     }
 
